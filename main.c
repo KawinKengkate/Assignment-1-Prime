51a56d3ed6fbbab5f74d2d21e530daf259d0262e
KawinKengkate <97278120+KawinKengkate@users.noreply.github.com>
2022-01-12 15:51:45 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+int check(int);
+
+int main(int argc, char *argv[] ) 
+{
+  int i, N, temp, j = 0;
+  int *a, *b;
+  N = argc - 1;
+  a = (int*) malloc (sizeof(int)*N); //รับargvทั้งหมด
+  b = (int*) malloc (sizeof(int)*N); //รับprime argv
+  
+  for(i = 1; i < argc; i++)
+  {
+    a[i-1] = atoi(argv[i]);
+    if(check(a[i-1]) == 1)
+    {
+        b[j] = a[i-1];
+        j++;
+    }
+  }
+  
+  for(i = 0; i < j; i++) //sort
+  {
+    for(int k = i+1; k < j; k++)
+    {
+      if(b[i]>b[k])
+      {
+        temp = b[i];
+        b[i] = b[k];
+        b[k] = temp;
+      }
+    }
+  }
+
+  printf("The smallest prime numbers is %d\n",b[0]);
+  printf("The biggest  prime numbers is %d\n",b[j-1]);
+
+  return 0;
+}
+
+int check(int n)
+{
+  if(n<=1)
+  {
+    return 0;
+  }
+  for(int i=n-1; i > 1; i--)
+  {
+    if(n%i == 0)
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
